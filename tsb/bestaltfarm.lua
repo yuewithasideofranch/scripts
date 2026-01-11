42cfdbd555865de18988d5a28d42a7f77196f99b
yue <yuewithasideofranch@gmail.com>
2026-01-10 20:21:44 -0700

    Update bestaltfarm.lua

--- tsb/bestaltfarm.lua
+++ tsb/bestaltfarm.lua
@@ -4,6 +4,10 @@ if not game:IsLoaded() then
     game.Loaded:Wait() 
 end
 
+local args = {...}
+local controller = args[1].controller
+local mainAccount = game:GetService("Players"):FindFirstChild(controller["MainAccount"])
+
 local lp = game.Players.LocalPlayer
 local savedPos = nil
 local farming = false
@@ -52,46 +56,7 @@ end
 
 spawn(farmLoop)
 
-for _,player in ipairs(game.Players:GetPlayers()) do
-    player.Chatted:Connect(function(msg)
-        msg = msg:lower()
-        if player ~= lp then
-            if msg == ".sp" then
-                if player.Character and player.Character.PrimaryPart then
-                    savedPos = player.Character.PrimaryPart.Position
-                    print("[.setpos] Saved position of " .. player.Name .. ":", savedPos)
-                end
-            end
-            if msg == ".br" then
-                if savedPos then
-                    teleport(savedPos)
-                    print("[.bring] Teleported")
-                else
-                    print("[.bring] No position saved.")
-                end
-            end
-            if msg == ".fa" then
-                if savedPos then
-                    farming = true
-                    print("[.farm] Started farming loop.")
-                else
-                    print("[.farm] No position saved.")
-                end
-            end
-            if msg == ".st" then
-                farming = false
-                print("[.stop] Stopped farming.")
-            end   
-            if msg == "autopls" then
-                if player.Character and player.Character.PrimaryPart then
-                    savedPos = player.Character.PrimaryPart.Position
-                    print("[.autopls] Saved position of " .. player.Name .. ":", savedPos)
-                end
-                farming = true
-                print("[.autopls] Auto setup.")
-            end 
-            if msg == ".ra" then
-                local UserSettings = UserSettings()
+local UserSettings = UserSettings()
                 UserSettings.GameSettings.MasterVolume = 0
                 game:GetService("RunService"):Set3dRenderingEnabled(false)
                 game:GetService("Players").LocalPlayer.PlayerGui:Destroy()
@@ -102,8 +67,9 @@ for _,player in ipairs(game.Players:GetPlayers()) do
                 end
                 end
                 loadstring(game:HttpGet("https://raw.githubusercontent.com/evxncodes/mainroblox/main/anti-afk", true))() --anti afk
-                print("[.ram] Removed assets & more.")
-            end     
-        end
-    end)
-end
+
+                    savedPos = mainAccount.Character.PrimaryPart.Position
+                    print("[.autopls] Saved position of " .. player.Name .. ":", savedPos)
+                end
+                farming = true
+                print("[.autopls] Auto setup.")
